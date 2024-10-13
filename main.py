361fded23dc9a02029fc6e62af156c744e41f5f8
TreasureCoded-locked-in <akpannsikak410@gmail.com>
2024-10-13 16:47:11 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Creating different Vaiables
+x = 5
+y = 'John'
+print(x)
+print(y)
+
+codingal='codingal'
+print(codingal)
+
+#User Input
+name = input('Enter your name : ')
+
+print('\nHello', name, '\nWelcome to codingal' )
